67f92f3d958fc68d99f3be9fd550b2075037e101
Quang Vu <qdv2130@rit.edu>
2015-11-02 22:12:27 -0500

    Recursively traverse through a sample tree

--- src/TreeAndHeap/TraverseTree.java
+++ src/TreeAndHeap/TraverseTree.java
@@ -3,23 +3,66 @@ import java.util.*;
 
 /**
  * Created by Quang on 11/2/15.
+ * Recursively traverse through a sample tree in-order, pre-order, and post-order
  */
 public class TraverseTree {
+    private ArrayList<Integer> iOrder = new ArrayList<Integer>();
+    private ArrayList<Integer> preOrd = new ArrayList<Integer>();
+    private ArrayList<Integer> postOrd = new ArrayList<Integer>();
+
+    /*
+     * Recursive in-order
+     */
     public ArrayList<Integer> inOrder(TreeNode root){
-        return null;
+        if (root != null) {
+            inOrder(root.left);
+            iOrder.add(root.value);
+            inOrder(root.right);
+        }
+
+        return iOrder;
     }
 
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length of the tree");
-        int length = sc.nextInt();
-
-        for(int i = 0; i < length; i++){
-            System.out.println("Enter a value for your tree");
-            int value = sc.nextInt();
-
-            TreeNode node = new TreeNode(value);
-
+    public ArrayList<Integer> preOrder(TreeNode root){
+        if (root != null){
+            preOrd.add(root.value);
+            preOrder(root.left);
+            preOrder(root.right);
         }
+
+        return preOrd;
+    }
+
+    public ArrayList<Integer> postOrder(TreeNode root){
+        if(root != null){
+            postOrder(root.left);
+            postOrder(root.right);
+            postOrd.add(root.value);
+        }
+
+        return postOrd;
+    }
+    public static void main(String args[]){
+        TraverseTree tTree = new TraverseTree();
+
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+
+        n3.left = n2;
+        n3.right = n4;
+        n4.right = n5;
+        n5.left = null;
+        n5.right = null;
+        n2.left = n1;
+        n2.right = null;
+        n1.left = null;
+        n1.right =null;
+
+        System.out.println("Inorder: " + tTree.inOrder(n3) +"\n");
+        System.out.println("Preorder: " + tTree.preOrder(n3) + "\n");
+        System.out.println("Postorder: " + tTree.postOrder(n3) + "\n");
     }
 }
