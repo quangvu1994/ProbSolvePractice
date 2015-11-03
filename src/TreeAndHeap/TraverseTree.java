863250749cd4e3ae23af47d2ec9ac5935be2ac10
Quang Vu <qdv2130@rit.edu>
2015-11-02 21:19:51 -0500

    Working with tree structure

---
+++ src/TreeAndHeap/TraverseTree.java
@@ -0,0 +1,25 @@
+package TreeAndHeap;
+import java.util.*;
+
+/**
+ * Created by Quang on 11/2/15.
+ */
+public class TraverseTree {
+    public ArrayList<Integer> inOrder(TreeNode root){
+        return null;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the tree");
+        int length = sc.nextInt();
+
+        for(int i = 0; i < length; i++){
+            System.out.println("Enter a value for your tree");
+            int value = sc.nextInt();
+
+            TreeNode node = new TreeNode(value);
+
+        }
+    }
+}
---
+++ src/TreeAndHeap/TreeNode.java
@@ -0,0 +1,18 @@
+package TreeAndHeap;
+
+/**
+ * Created by Quang on 11/2/15.
+ */
+/*
+ * Simple binary tree structure
+ */
+public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value){
+        this.value = value;
+    }
+}
+
