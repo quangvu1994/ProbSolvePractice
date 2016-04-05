205230dd2483ac3f24d3f5ced1dc324f3c3ca327
Quang Vu <qdv2130@rit.edu>
2016-04-04 21:55:57 -0400

    minor fix

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -8,13 +8,16 @@ public class InsertionSort {
 
     public int[] sort(int[] array){
         int temp;
-
         for(int i = 1; i < array.length; i++){
             for(int j = i; j > 0; j--){
+                // Swap with the smaller number
                 if(array[j] <  array[j-1]){
                     temp = array[j];
                     array[j] = array[j-1];
                     array[j-1] = temp;
+                }
+                else{
+                    break; //Skip unnecessary loop
                 }
             }
         }
