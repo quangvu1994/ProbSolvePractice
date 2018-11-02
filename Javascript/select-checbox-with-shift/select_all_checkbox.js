c6698ac77a32a5f5c5d1886bacbdcc3efe78e504
Quang Vu <qdv2130@rit.edu>
2018-11-01 23:23:27 -0400

    added select all method

--- Javascript/select-checbox-with-shift/select_all_checkbox.js
+++ Javascript/select-checbox-with-shift/select_all_checkbox.js
@@ -1,1 +1,26 @@
-const checkboxes = document.querySelectorAll('input');
+const inbox = document.querySelector('.inbox');
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+let last_checked;
+function handle_checkbox(e) {
+    // only handle the event if the target is input/checkbox type
+    if (e.target.tagName == 'INPUT') {
+        let trigger_checking = false;
+
+        if(e.target.checked && e.shiftKey) {
+            checkboxes.forEach(checkbox => {
+                if (checkbox === last_checked || checkbox === e.target) {
+                    trigger_checking = !trigger_checking;
+                }
+
+                if (trigger_checking) {
+                    checkbox.checked = true;
+                }
+            });
+        }
+        last_checked = e.target;
+    }
+
+    e.stopPropagation();
+}
+inbox.addEventListener('click', handle_checkbox);
